0d65bdf280a8af348cf41bd20a694f4fbc9f22d1
jotalevi <etalevi@moveapps.cl>
2024-01-23 00:21:36 -0300

    added lazy loading to banner image

--- src/eventbanner/eventbanner.js
+++ src/eventbanner/eventbanner.js
@@ -1,7 +1,14 @@
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 function EventBanner(props) {
   return (
     <div className="imagebannerdiv">
-      <img src="/currentbanner.png" className="imagebanner" />
+      <LazyLoadImage
+        alt="/currentbanner.png"
+        src="/currentbanner.png"
+        className="imagebanner"
+        effect="blur"
+      />
       <div className="gobt" onClick={props.onAction}>
         <h1 className="gobt_text">Comprar Entradas</h1>
       </div>
